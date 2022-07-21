07e8be2d006d6d5145aa0ea751ba09b5a380aa7b
TrueSunnyD <82911075+TrueSunnyD@users.noreply.github.com>
2022-07-21 11:36:32 -0600

    Updated SQL to latest oxmysql

--- server/sv_main.lua
+++ server/sv_main.lua
@@ -2,7 +2,7 @@ local dispatchCodes = {}
 
 AddEventHandler('onResourceStart', function(resource)
     if resource == GetCurrentResourceName() then
-        MySQL.Async.fetchAll('SELECT * FROM dispatch_codes', {}, function(result)
+        MySQL.scalar.await('SELECT * FROM dispatch_codes', {}, function(result)
             if not result then print("No results available from the DB") return end
             for k, v in pairs(result) do
                 print("Adding " .. v.display_code .. " to the table")
